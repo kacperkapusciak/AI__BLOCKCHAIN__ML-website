07c0fa18ca4fb3dc42ea4c1072df41b741fbf4db
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-20 11:46:35 +0100

    Basic react-router setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import React from "react";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import Landing from "components/Landing";
+import Contact from "components/Contact";
 
-export default () => <div>Hi</div>;
+const App = () => (
+  <>
+    <Switch>
+      <Route path="/contact" component={Contact} />
+      <Route path="/" exact component={Landing} />
+      <Redirect to="/" />
+    </Switch>
+  </>
+);
+
+export default withRouter(App);

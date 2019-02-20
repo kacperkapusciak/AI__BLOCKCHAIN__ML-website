6a3d89461e626fd48a9ffc15a048e5caff8c2ab1
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-02-20 12:28:16 +0100

    Added some global styling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import Landing from "components/Landing";
 import Contact from "components/Contact";
+import "./App.css";
+
+// font-awesome boilerplate
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHome, faPhone, faDesktop } from "@fortawesome/free-solid-svg-icons";
+library.add(faHome, faPhone, faDesktop);
 
 const App = () => (
-  <>
+  <div className="app">
     <Switch>
       <Route path="/contact" component={Contact} />
       <Route path="/" exact component={Landing} />
       <Redirect to="/" />
     </Switch>
-  </>
+  </div>
 );
 
 export default withRouter(App);
